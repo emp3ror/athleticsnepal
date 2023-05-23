4e4e92a841b6cd748c235857d445df73336e951b
mjt <thapa.manish16@gmail.com>
2023-05-23 09:21:29 +0545

    update meta title

--- src/app1/layout.tsx
+++ src/app1/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Athletes Nepal</title>
+        <meta name="keywords" content="athletes Nepal, Nepalese sports stars, sporting achievements, sports excellence, Nepali athletes, sports personalities, success stories, Nepal's sports icons"/>
+        <meta name="description" content="Discover the incredible athletes from Nepal, their achievements, and inspiring success stories. Explore the sporting excellence and talent that has emerged from Nepal's athletic community."/>
+        <meta name="author" content="Your Name"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
